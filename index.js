3d158fe6fda5bc03f6c48db49733020d75e73ada
dqisme <dqisme@outlook.com>
2018-12-20 14:03:06 +0800

    [dq] #2.1 implement with three states.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = {};
+var STATES = {
+  PENDING: 'pending',
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected'
+};
+
+module.exports = {
+  deferred: function () {
+    var promise = {
+      state: STATES.PENDING,
+      value: null,
+      reason: null
+    };
+    var resolve = function (value) {
+      if (promise.state === STATES.PENDING) {
+        promise.state = STATES.FULFILLED;
+        promise.value = value;
+      }
+    };
+    var reject = function (reason) {
+      if (promise.state === STATES.PENDING) {
+        promise.state = STATES.REJECTED;
+        promise.reason = reason;
+      }
+    };
+    return { promise: promise, reject: reject, resolve: resolve };
+  }
+};
